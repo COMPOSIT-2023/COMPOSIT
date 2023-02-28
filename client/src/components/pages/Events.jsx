6d242ff3f981a20407eac020417cc6fff7a27c10
soumili202 <soumilirupsa2020@gmail.com>
2023-02-28 23:38:00 +0530

    Events page added+editted

--- client/src/components/pages/Events.jsx
+++ client/src/components/pages/Events.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import MainBanner from '../Events/MainBanner';
-
+import lax from 'lax.js';
+import GoTop from '../Shared/GoTop';
 import WhyUs from '../Common/WhyUs';
 import Footer from '../Common/Footer';
-class AboutPage extends React.Component {
+class EventPage extends React.Component {
     constructor(props) {
         super(props)
         lax.setup()
@@ -35,4 +36,60 @@ class AboutPage extends React.Component {
     }
 }
  
-export default AboutPage;
+export default EventPage;
+
+
+// import React from 'react';
+// import MainBanner from '../About/MainBanner';
+// import About from '../About/About';
+// import WhyUs from '../Common/WhyUs';
+// import Speakers from '../About/Speakers';
+// import GoTop from '../Shared/GoTop';
+// import Footer from '../Common/Footer';
+// import FunFact from '../Common/FunFact';
+// import lax from 'lax.js';
+// import Partner from '../Common/Partner';
+// import Subscribe from '../Common/Subscribe';
+// import Cta from '../Common/Cta';
+ 
+// class AboutPage extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         lax.setup()
+    
+//         document.addEventListener('scroll', function(x) {
+//             lax.update(window.scrollY)
+//         }, false)
+    
+//         lax.update(window.scrollY)
+//     }
+//     render(){
+//         return (
+//             <React.Fragment>
+//                 {/* Main Banner */}
+//                 <MainBanner />
+//                 {/* About Area */}
+//                 <About />
+//                 {/* Why Choose Us Area */}
+//                 <WhyUs />
+//                 {/* Speakers Area */}
+//                 <Speakers />
+//                 {/* CTA Are */}
+//                 <Cta />
+//                 {/* FunFacts Area */}
+//                 <FunFact />
+//                 {/* Pricing Area */}
+//                 <Partner />
+//                 {/* Buy Tickets Area */}
+//                 <Subscribe />
+
+//                 <Footer />
+
+//                 {/* Back Top top */}
+//                 <GoTop scrollStepInPx="50" delayInMs="16.66" />
+//             </React.Fragment>
+//         );
+//     }
+// }
+ 
+// export default AboutPage;

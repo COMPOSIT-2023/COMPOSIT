0d5594069ccf8d0168fe1defb64d7b52ea6323c5
Mayur25Lohani <lohanimayuresh2551@gmail.com>
2023-03-10 16:24:20 +0530

    Created Events Page

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -124,6 +124,16 @@ class Navigation extends React.Component {
 
                                     <li className="nav-item">
                                         <NavLink 
+                                            to="/eventdetails" 
+                                            className="nav-link" 
+                                            onClick={this.toggleNavbar}
+                                        >
+                                            EventPage
+                                        </NavLink>
+                                    </li>
+
+                                    <li className="nav-item">
+                                        <NavLink 
                                             to="/sponsors" 
                                             className="nav-link" 
                                             onClick={this.toggleNavbar}

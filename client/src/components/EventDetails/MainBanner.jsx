7ea8d6e3cfd7e3acd83005eec465691be2336beb
anuraganand92 <anuragsr26@gmail.com>
2023-03-11 22:41:36 +0530

    fixed event cards

--- client/src/components/EventDetails/MainBanner.jsx
+++ client/src/components/EventDetails/MainBanner.jsx
@@ -8,6 +8,11 @@ class MainBanner extends React.Component {
                 <div className="container">
                     <h1>Event Details</h1>
                     <span>Are you ready to participate?</span>
+                    <ul>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/events">Events</Link></li>
+                        <li>Event Details</li>
+                    </ul>
                 </div>
             </div>
         );
